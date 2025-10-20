5e63dd93898f086ad6f56fd6228cd9cb91082c28
WEVOTEAM\damral.gara <damral.gara@wevo.az>
2025-10-20 16:16:16 +0400

    get info from section

--- Az204/Controllers/WeatherForecastController.cs
+++ Az204/Controllers/WeatherForecastController.cs
@@ -1,45 +1,48 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 
-namespace Az204.Controllers
+namespace Az204.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    private readonly IConfiguration _configuration;
+    private static readonly string[] Summaries = new[]
     {
-        private readonly IConfiguration _configuration;
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+    private readonly ILogger<WeatherForecastController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger, IConfiguration configuration)
-        {
-            _logger = logger;
-            _configuration = configuration;
-        }
-
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
-        }
-
-        [HttpGet("/version")]
-        public string GetAppVersion()
-        {
-            return _configuration.GetValue<string>("AppVersion") ?? "1.00a";
-
-        }
-
-
+    public WeatherForecastController(ILogger<WeatherForecastController> logger, IConfiguration configuration)
+    {
+        _logger = logger;
+        _configuration = configuration;
     }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+
+    [HttpGet("/version")]
+    public AppSetting GetAppVersion()
+    {
+        return _configuration.GetRequiredSection("AppInfo").Get<AppSetting>() ?? new AppSetting();
+    }
+}
+
+public class AppSetting
+{
+    public string Name { get; set; } = string.Empty;
+    public string Version { get; set; } = string.Empty;
+    public string Sign { get; set; } = string.Empty;
 }
